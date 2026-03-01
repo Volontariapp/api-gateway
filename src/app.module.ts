eee7f1b02cdadef6d8e275c16ae232fdc73e9626
VictorAgahi <victor@agahi.com>
2026-03-01 16:18:49 +0100

    feat: scaffold project architecture with wired modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { AppConfigModule } from './config/app-config.module';
+import { GrpcClientModule } from './grpc/grpc-client.module';
+import { UserModule } from './modules/user/user.module';
+import { PostModule } from './modules/post/post.module';
+import { EventModule } from './modules/event/event.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    AppConfigModule,
+    GrpcClientModule,
+    UserModule,
+    PostModule,
+    EventModule,
+  ],
 })
 export class AppModule {}
