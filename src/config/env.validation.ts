1218f8ad4ae9cfda7dd2acc53c9f020b6d3ba2c1
VictorAgahi <victor@agahi.com>
2026-04-07 13:50:33 +0200

    feat(auth): adding auth lib

--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -6,7 +6,7 @@ export const envValidationSchema = Joi.object({
     .default('development'),
   PORT: Joi.number().default(3000),
 
-  MS_USER_URL: Joi.string().default('0.0.0.0:5001'),
-  MS_POST_URL: Joi.string().default('0.0.0.0:5002'),
-  MS_EVENT_URL: Joi.string().default('0.0.0.0:5003'),
+  MS_USER_URL: Joi.string().default('localhost:5001'),
+  MS_POST_URL: Joi.string().default('localhost:5002'),
+  MS_EVENT_URL: Joi.string().default('localhost:5003'),
 });
