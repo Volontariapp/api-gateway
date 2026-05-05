feb52829010e2dacfca90f2c0edd1eebc4fcfb1b
VictorAgahi <victor@agahi.com>
2026-05-05 11:49:41 +0200

    refacto(archi): controllers clean archi + decorator

--- src/modules/post/dto/request/update-post.request.dto.ts
+++ src/modules/post/dto/request/update-post.request.dto.ts
@@ -1,12 +1,17 @@
 import { ApiProperty, PartialType, OmitType } from '@nestjs/swagger';
 import type { UpdatePostCommand } from '@volontariapp/contracts-nest';
 import { CreatePostRequestDTO } from './create-post.request.dto.js';
+import type { UpdatePostRequest } from '@volontariapp/contracts';
 
-export class UpdatePostRequestDTO extends PartialType(
-  OmitType(CreatePostRequestDTO, ['toCommand'] as const),
-) {
+export class UpdatePostRequestDTO
+  extends PartialType(OmitType(CreatePostRequestDTO, ['toCommand'] as const))
+  implements UpdatePostRequest
+{
   @ApiProperty({ example: 'uuid-123' })
   id!: string;
+
+  post?: UpdatePostRequest['post']; // Ne pas garder cette ecritre c'est interdit
+  updateMask?: UpdatePostRequest['updateMask'];
 
   toCommand(): UpdatePostCommand {
     const updateMask: string[] = [];
