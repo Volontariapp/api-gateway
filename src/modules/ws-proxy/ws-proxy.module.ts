452322eb1d7e6715036b7c0a3b4703c9ec7f45ae
VictorAgahi <victor@agahi.com>
2026-06-02 13:25:17 +0200

    fix: WebSockets proxy bypassing NestJS router due to missing controller

--- src/modules/ws-proxy/ws-proxy.module.ts
+++ src/modules/ws-proxy/ws-proxy.module.ts
@@ -1,9 +1,11 @@
 import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { WsProxyMiddleware } from './ws-proxy.middleware.js';
 import { AuthModule } from '@volontariapp/auth';
+import { WsProxyController } from './ws-proxy.controller.js';
 
 @Module({
   imports: [AuthModule],
+  controllers: [WsProxyController],
   providers: [WsProxyMiddleware],
 })
 export class WsProxyModule implements NestModule {
