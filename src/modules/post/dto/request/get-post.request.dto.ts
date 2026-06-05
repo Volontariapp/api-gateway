9ddf0c422a6b9fc2a3207fc02afbbd45387bf800
victor.giroud <victor.giroud@epita.fr>
2026-06-05 14:49:53 +0200

    ci correction

--- src/modules/post/dto/request/get-post.request.dto.ts
+++ src/modules/post/dto/request/get-post.request.dto.ts
@@ -1,6 +1,7 @@
 import type { GetPostQuery } from '@volontariapp/contracts-nest';
 import type { GetPostRequest } from '@volontariapp/contracts';
 
+//GetPostRequestDto
 export class GetPostRequestDTO implements GetPostRequest {
   id!: string;
 
