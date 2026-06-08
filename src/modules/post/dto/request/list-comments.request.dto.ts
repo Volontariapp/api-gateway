f54f0570685fbe5cb0cd7f1d05c58d8014e63bfe
VictorAgahi <victor@agahi.com>
2026-06-08 12:41:50 +0200

    patch api-gateway

--- src/modules/post/dto/request/list-comments.request.dto.ts
+++ src/modules/post/dto/request/list-comments.request.dto.ts
@@ -1,22 +1,16 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsInt, IsOptional, Min } from 'class-validator';
+
 import { ListCommentsQuery } from '@volontariapp/contracts-nest';
 import type { ListCommentsRequest } from '@volontariapp/contracts';
 
 export class ListCommentsRequestDTO implements ListCommentsRequest {
   @ApiPropertyOptional({ example: 1, default: 1 })
   @Transform(({ value }) => parseInt(value as string, 10))
-  @IsInt()
-  @Min(1)
-  @IsOptional()
   page: number = 1;
 
   @ApiPropertyOptional({ example: 10, default: 10 })
   @Transform(({ value }) => parseInt(value as string, 10))
-  @IsInt()
-  @Min(1)
-  @IsOptional()
   limit: number = 10;
 
   postId!: string;
--- src/modules/post/dto/request/list-posts.request.dto.ts
+++ src/modules/post/dto/request/list-posts.request.dto.ts
@@ -1,22 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsInt, IsOptional, Min } from 'class-validator';
 import { ListPostsQuery } from '@volontariapp/contracts-nest';
 import { ListPostsRequest } from '@volontariapp/contracts';
 
 export class ListPostsRequestDTO implements ListPostsRequest {
   @ApiProperty({ example: 10, required: false })
-  @IsOptional()
   @Type(() => Number)
-  @IsInt()
-  @Min(1)
   limit?: number;
 
   @ApiProperty({ example: 1, required: false })
-  @IsOptional()
   @Type(() => Number)
-  @IsInt()
-  @Min(1)
   page?: number;
 
   @ApiProperty({ example: 'uuid-user-123', required: false })
