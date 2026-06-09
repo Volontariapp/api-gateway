7388174bc24ea5dcc54e2f47e3ba6addee78a575
VictorAgahi <victor@agahi.com>
2026-06-09 14:42:16 +0200

    feat(post): patch event created insertion

--- src/modules/post/dto/request/create-post.request.dto.ts
+++ src/modules/post/dto/request/create-post.request.dto.ts
@@ -9,10 +9,14 @@ export class CreatePostRequestDTO implements CreatePostRequest {
   @ApiProperty({ example: 'My first post' })
   content!: string;
 
+  @ApiProperty({ example: 'event-id', required: false })
+  eventId?: string;
+
   toCommand(): CreatePostCommand {
     return {
       title: this.title,
       content: this.content,
+      eventId: this.eventId,
     };
   }
 }
